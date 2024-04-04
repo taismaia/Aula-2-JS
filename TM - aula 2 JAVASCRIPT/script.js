1e24278df8dbe8efc11f7b8aeccdcde92e19ac4e
taismaia <148501573+taismaia@users.noreply.github.com>
2024-04-04 18:59:02 -0300

    Add files via upload

---
+++ TM - aula 2 JAVASCRIPT/script.js
@@ -0,0 +1,110 @@
+
+// DESAFIO 
+
+// const valor_hora = Number(prompt("Digite quanto você ganha por hora: "))
+// const qtde_horas = Number(prompt("Digite quantas horas você trabalha por mês: "))
+
+// const salario_bruto = qtde_horas * valor_hora
+// const desconto_IR = salario_bruto * 0.11
+// const desconto_INSS = salario_bruto * 0.08
+// const desconto_sindicato = salario_bruto * 0.05
+// const salario_liquido = salario_bruto - desconto_INSS - desconto_IR - desconto_sindicato
+
+// console.log(`Salário bruto: ${salario_bruto}`)
+// console.log(`Desconto do Imposto de Renda: ${desconto_IR}`)
+// console.log(`Desconto do INSS: ${desconto_INSS}`)
+// console.log(`Desconto do Sindicato: ${desconto_sindicato}`)
+// console.log(`Salário liquido: ${salario_liquido}`)
+
+
+// OPERADOR DE COMPARAÇÃO
+
+// const numero1 = 5
+// const numero2 = 8
+// const numero3 = "5"
+// console.log(numero1 == numero2)
+// console.log(numero1 == numero3)
+// console.log(numero1 === numero3)
+// console.log(numero1 != numero3)
+// console.log(numero1 !== numero3)
+// console.log(numero1 > numero2)
+// console.log(numero1 < numero2)
+// console.log(numero1 >= numero2)
+// console.log(numero1 <= numero2)
+
+
+// CONDICIONAIS IF/ELSE
+
+// const idade = Number(prompt("Digite sua idade: "))
+
+
+// if( (idade >= 16 && idade < 18) || (idade > 70)){
+//     alert("Voto  facultativo")
+// }else if(idade >= 18 && idade <= 70){
+//     alert("Voto obrigatório")
+// }else{
+//     alert("Você não pode votar")
+// }
+
+
+
+// const numero = Number(prompt("Digite um número: "))
+
+// if (numero > 0){
+//     alert("Positivo")
+// }else if(numero < 0){
+//     alert("Negativo")
+// }else{
+//     alert("O número é neutro")
+// }
+
+
+// const horario = Number(prompt("Digite uma hora do dia: "))
+
+// if(horario >= 5 && horario <= 11){
+//     alert("Bom dia")
+// }else if (horario >= 12 && horario <= 17){
+//     alert("Boa tarde")
+// }else if ((horario >= 18 && horario <= 23) || (horario >= 0 && horario <= 4)){
+//     alert("Boa noite")
+// }else{
+//     alert("Horário inválido")
+// }
+
+
+// const numero = Number(prompt("Digite um número: "))
+
+// numero % 2 === 0 ? alert("Número par") : alert("Número ímpar")
+
+
+// const cor = prompt("Digite uma cor do semáforo: ")
+
+// switch(cor){
+//     case "vermelho":
+//         alert("PARE!")
+//         break
+//     case "amarelo":
+//         alert("ATENÇÃO!")
+//         break
+//     case "verde":
+//         alert("PROSSIGA!")
+//         break
+//     default:
+//         alert("Opção inválida")
+//         break
+// }
+
+
+
+// const numero = Number(prompt("Digite um número: "))
+
+// if(numero > 0 && numero % 2 == 0){
+//     alert("Positivo")
+// }else if (numero < 0){
+//     alert("Negativo")
+// }
+
+
+const altura = Number(prompt("Digite sua altura: "))
+const peso = Number(prompt("Digite seu peso: "))
+
